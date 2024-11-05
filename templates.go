3bb48371be9bca63267cbd102cec06585e38a630
davidpkj <davidpenkow1@gmail.com>
2024-11-05 15:02:42 +0100

    Specialize (i.e.: remove most) output fields

--- templates.go
+++ templates.go
@@ -2,26 +2,18 @@ package ics
 
 const ics = `BEGIN:VCALENDAR
 PRODID:{{.ProdId}}
-METHOD:REQUEST
 VERSION:2.0
 {{range $ve := .Events}}{{$ve}}
 {{end}}END:VCALENDAR`
 
 const vevent = `BEGIN:VEVENT
-ORGANIZER;CN="{{.Organizer.CommonName}}":mailto:{{.Organizer.EmailAddress}}
-{{range $at := .Attendees}}ATTENDEE;CUTYPE={{$at.CuType}};ROLE={{$at.Role}};PARTSTAT={{$at.PartStatus}};CN="{{$at.CommonName}}";RSVP={{$at.Rsvp}}:mailto:{{$at.EmailAddress}}
-{{end}}LOCATION:{{.Location}}
-DTSTAMP;TZID={{.TZid}}:{{.DtStamp}}
-DTSTART;TZID={{.TZid}}:{{.DtStart}}
-DTEND;TZID={{.TZid}}:{{.DtEnd}}{{range $ru := .RRule}}
-RRULE:{{$ru}}{{end}}{{range $ru := .ExRule}}
-EXRULE:{{$ru}}{{end}}{{range $ru := .ExDate}}
-EXDATE:{{$ru}}{{end}}
+LOCATION:{{.Location}}
+DTSTAMP:{{.DtStamp}}
+DTSTART:{{.DtStart}}
+DTEND:{{.DtEnd}}{{range $ru := .RRule}}
+RRULE:{{$ru}}{{end}}
 SUMMARY:{{.Summary}}
-DESCRIPTION:{{.Description}}
-CLASS:{{.Class}}
 UID:{{.UID}}
-STATUS:{{.Status}}
 END:VEVENT`
 
 type vEvent struct {
